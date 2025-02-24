5c018040b0233286684acfd93127451add356cb6
Prince Sharma <connect.princesharma@gmail.com>
2025-02-25 00:55:49 +0530

    Project Completed

---
+++ build/CMakeFiles/simpleShell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleShell.dir/simpleShell.c.o"
+  "CMakeFiles/simpleShell.dir/simpleShell.c.o.d"
+  "simpleShell"
+  "simpleShell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simpleShell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/simpleShell.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for simpleShell.
+# This may be replaced when dependencies are built.
